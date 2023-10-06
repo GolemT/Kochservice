0f9d101b2db80820872dcf759ae4ef66b0726474
GolemT <tkosleck@gmail.com>
2023-10-06 18:41:43 +0200

    setup

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Kochservice
+
+## Good to know:
+
+### index
+Main Page, auf der alle weiteren Websiten angezeigt werden
+
+### rezepte
+Gesamtübersicht aller verfügbaren Rezepte auf der Website
+
+### gericht
+Page mit einem einzelnen Gericht, zeigt die Zubereitung an
+
+### vorschlaege
+Kontaktvormular um neue Rezepte anzufordern
+
+### kontakt
+Impressum und Danksagung an Entwickler
+
+## How to Setup/Run
+Es wird Node.js gebraucht um die Anwendung zu starten.
+Um die Website zum laufen zu bringen zuerst
+    
+    npm install
+
+und im Anschluss:
+
+    npm run dev
+
+Nun ist die Website aufrufbar über https://localhost:3000
---
+++ pages/rezepte.js
@@ -0,0 +1,130 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+ 
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Kochservice</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      <Link href="/">GolemT's Kochservice!</Link>
+        <h1 className={styles.title}>
+          Rezepte!
+        </h1>
+
+
+        <div className={styles.grid}>
+          <Link href="/rezepte.js" className={styles.card}>
+            <h3>Rezepte &rarr;</h3>
+            <p>Eine Übersicht aller Rezepte.</p>
+          </Link>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Random &rarr;</h3>
+            <p>Finde ein Rezept auf gut glück.</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h3>Vorschläge &rarr;</h3>
+            <p>Schlage neue Rezepte vor und erweitere unser Sortiment.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Kontakt &rarr;</h3>
+            <p>
+              Unser Impressum sowie alle beteiligten Personen.
+            </p>
+          </a>
+        </div>
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
