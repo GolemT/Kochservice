1c747bdb7d903efbe953e6f22e3f3acd57d1dec0
GolemT <tkosleck@gmail.com>
2023-12-12 13:29:21 +0100

    added Layout for easy page writing

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,7 @@
-import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { getSearch }  from '../components/logic.js'
-import Navbar from '../components/navbar';
 import { useRouter } from 'next/router';
+import Layout from '../components/Layout.js';
 
 
 export default function search(){
@@ -12,14 +11,8 @@ export default function search(){
     const recipes = getSearch(input)
 
     return (
-        <div className={styles.container}>      
-          <Head>
-            <title>Kochservice</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-    
+        <Layout>
           <main>
-            <Navbar />
             <h1 className={styles.title}>
               Rezepte mit '{input}'
             </h1>
@@ -31,84 +24,6 @@ export default function search(){
     
             </div>
           </main>
-    
-          <footer>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Powered by{' '}
-              <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-            </a>
-          </footer>
-    
-          <style jsx>{`
-            main {
-              padding: 5rem 0;
-              flex: 1;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-            }
-            footer {
-              width: 100%;
-              height: 100px;
-              border-top: 1px solid #eaeaea;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-            }
-            footer img {
-              margin-left: 0.5rem;
-            }
-            footer a {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              text-decoration: none;
-              color: inherit;
-            }
-            code {
-              background: #fafafa;
-              border-radius: 5px;
-              padding: 0.75rem;
-              font-size: 1.1rem;
-              font-family:
-                Menlo,
-                Monaco,
-                Lucida Console,
-                Liberation Mono,
-                DejaVu Sans Mono,
-                Bitstream Vera Sans Mono,
-                Courier New,
-                monospace;
-            }
-          `}</style>
-    
-          <style jsx global>{`
-            html,
-            body {
-              padding: 0;
-              margin: 0;
-              font-family:
-                -apple-system,
-                BlinkMacSystemFont,
-                Segoe UI,
-                Roboto,
-                Oxygen,
-                Ubuntu,
-                Cantarell,
-                Fira Sans,
-                Droid Sans,
-                Helvetica Neue,
-                sans-serif;
-            }
-            * {
-              box-sizing: border-box;
-            }
-          `}</style>
-        </div>
+        </Layout>
       );
 }
