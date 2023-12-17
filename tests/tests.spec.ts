ea64e4d49af4bae079d50f73466a9746438fd208
GolemT <tkosleck@gmail.com>
2023-12-17 15:37:39 +0100

    fixed DB overloading

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -1,5 +1,4 @@
 import { test, expect } from '@playwright/test';
-import data from '../components/data.json';
 
 test('has title', async ({ page }) => {
   await page.goto('/');
@@ -31,11 +30,6 @@ test('Inspect recipe', async ({ page }) => {
   await page.getByRole('link', { name: 'Zebrakuchen' }).click();
 
   await expect(page.getByRole('heading', { name: 'Zebrakuchen' })).toBeVisible();
-
-  const document = await page.getByText('300g Mehl, 1 Pck. Backpulver', {});
-
-  await expect(document).toBeVisible();
-  await expect(document).toHaveText(data[5].ingredients);
 })
 
 test('check navbar', async ({ page }) => {
@@ -43,5 +37,4 @@ test('check navbar', async ({ page }) => {
 
   await expect(page.getByRole('img', { name: "menu" })).toBeVisible()
   await expect(page.getByRole('textbox', {})).toBeVisible()
-
 })
